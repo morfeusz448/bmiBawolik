83fc9c70fa9debf4ddd32bcd26602adba477f4db
morfeusz448 <krzysztofbawolik@gmail.com>
2021-10-18 13:40:01 +0200

    Commit srodek pracy

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,25 @@ public class Main {
     public static void main(String[] args) throws IOException {
         float wzrostBawolik, wagaNumberBawolik, bmiBawolik;
 
-        System.out.println("Podaj wzrost");
+        System.out.println("Podaj wzrost w cm");
         BufferedReader nrbawolik1 = new BufferedReader(new InputStreamReader(System.in));
         wzrostBawolik = Float.parseFloat(nrbawolik1.readLine());
+        wzrostBawolik = wzrostBawolik / 100;
         System.out.println("Podaj wagę");
         BufferedReader nrbawolik2 = new BufferedReader(new InputStreamReader(System.in));
         wagaNumberBawolik = Float.parseFloat(nrbawolik2.readLine());
-        bmiBawolik = wagaNumberBawolik / (float) Math.pow(wzrostBawolik , 2);
+        bmiBawolik = wagaNumberBawolik / (float) Math.pow(wzrostBawolik, 2);
+
+        if (bmiBawolik > 16)
+        {
+            System.out.println("<16 wychudzenie");
+        } else if (16.00 >= bmiBawolik && bmiBawolik <= 16.99)
+        {
+            System.out.println("17.00 >= niedowaga <= 17.99");
+        } else if (17.00 >= bmiBawolik && bmiBawolik <= 17.99)
+        {
+            System.out.println("");
+    }
 
         System.out.println(bmiBawolik);
 
